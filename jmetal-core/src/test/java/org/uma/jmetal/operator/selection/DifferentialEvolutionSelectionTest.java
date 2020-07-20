4b89bea72d024fd03243c22b4ced988c277b6427
Matthieu Vergne <matthieu.vergne@gmail.com>
2020-07-20 20:22:19 +0200

    [WARNINGS] Replace deprecated items

--- jmetal-core/src/test/java/org/uma/jmetal/operator/selection/DifferentialEvolutionSelectionTest.java
+++ jmetal-core/src/test/java/org/uma/jmetal/operator/selection/DifferentialEvolutionSelectionTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
